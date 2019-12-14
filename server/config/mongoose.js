26c95d3c1868170509a2ffd9c3a30b7eb9cb8ea3
Andrew <michael.a.williams@mawfia.com>
2019-12-13 19:07:13 -0500

    Cleaning remote repo

--- server/config/mongoose.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/bicycle_marketplace', { useNewUrlParser: true });
-
-module.exports = mongoose;
--- src/app/image.ts
+++
@@ -1,7 +0,0 @@
-export class Image {
-	constructor(
-	public data: any = null,
-    public created_at: Date = new Date(),
-    public updated_at: Date = new Date()
-  ){}
-}
